b5f5cad224171a9b60ef38c75360a6efdf139049
tim-speight <60938034+tim-speight@users.noreply.github.com>
2024-02-19 10:25:28 +0000

    Start of week

--- docs/BlazorApexCharts.Docs/Data/RealTimeSeries.cs
+++ docs/BlazorApexCharts.Docs/Data/RealTimeSeries.cs
@@ -6,10 +6,10 @@ using System.Linq;
 
 namespace BlazorApexCharts.Docs
 {
-
     public class RealTimeSeriesGenerator
     {
         public List<RealTimeSeries> TimeSeries { get; set; } = new();
+        public List<RealTimeSeries> BaselineSeries { get; set; } = new();
 
         public long Range { get; private set; }
 
@@ -39,8 +39,12 @@ namespace BlazorApexCharts.Docs
             return new RealTimeSeries { Date = newDate, Value = Convert.ToDecimal(value), Quantity = rnd.Next(1, 20) };
         }
 
-
-    }
+        public RealTimeSeries GenerateBasePoint(DateTimeOffset newDate, double value)
+        {
+          var rnd = new Random();
+          return new RealTimeSeries { Date = newDate, Value = 0};
+        }
+  }
 
     public class RealTimeSeries
     {
